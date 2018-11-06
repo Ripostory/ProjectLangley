4a9a22044bd0a47767c4763af8a48868f8e3df81
Ripostory <ripostory@gmail.com>
2018-11-05 20:08:57 -0800

    Added destructible building materials

---
+++ Assets/Breakable.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Breakable : MonoBehaviour {
+    //functionality to destroy object based on velocity and number of times hit
+    public int hitPoints = 1;
+    public float velocityThreshold = 1.0f;
+    public UnityEvent onSlap;
+    public UnityEvent onBreak;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //check if exceeds damage threshold
+        string enemyTag = collision.gameObject.tag;
+        string myTag = gameObject.tag;
+        if ((myTag == "Structure" && enemyTag == "Ball") || (myTag != "Structure"))
+        {
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+                float impulse = collision.GetContact(i).normalImpulse;
+                if (impulse > velocityThreshold)
+                {
+                    Slap();
+                }
+            }
+        }
+    }
+
+    public void Slap()
+    {
+        //on significant damage
+        hitPoints--;
+        if (hitPoints < 0)
+        {
+            //Alert listeners that object broke
+            onBreak.Invoke();
+            //object broke, destroy.
+            Destroy(gameObject);
+        } else
+        {
+            //alert listeners that object was hit
+            onSlap.Invoke();
+        }
+    }
+}
